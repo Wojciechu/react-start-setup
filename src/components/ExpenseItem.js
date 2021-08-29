436eb5e0b06e94817868891b476949d1af8356eb
woyt4z <woytaz69@gmail.com>
2021-08-29 10:16:09 +0200

    35 output dynamic data35 output dynamic data35 output dynamic data35 output dynamic data35 output dynamic data35 output dynamic data35 output dynamic data35 output dynamic data35 output dynamic data35 output dynamic data35 output dynamic data35 output dynamic data35 output dynamic data35 output dynamic data35 output dynamic data35 output dynamic data35 output dynamic data35 output dynamic data35 output dynamic data35 output dynamic data35 output dynamic data35 output dynamic data35 output dynamic data35 output dynamic data35 output dynamic data35 output dynamic data35 output dynamic data35 output dynamic data35 output dynamic data35 output dynamic data35 output dynamic data35 output dynamic data35 output dynamic data35 output dynamic data35 output dynamic data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import './ExpenseItem.css'
 
 function ExpenseItem() {
+  const expenseDate = new Date(2021, 4, 2)
+  const expenseTitle = 'Battery change'
+  const expenseCost = 499
+
   return (
     <div className='expense-item'>
-      <div>2nd May 2021</div>
+      <div>{expenseDate.toDateString()}</div>
       <div className='expense-item__description'>
-        <h2>Battery change</h2>
-        <div className='expense-item__price'>499zł</div>
+        <h2>{expenseTitle}</h2>
+        <div className='expense-item__price'>{expenseCost}zł</div>
       </div>
     </div>
   );
